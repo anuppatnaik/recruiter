a5299f3d9cd480c28192b971cb7d61e9c95f8863
anuppatnaik <anup.patnaik@gmail.com>
2013-03-30 00:53:35 +0530

    Split index.php into respective views files. Re-did the UI. Minor fixes.

---
+++ src/views/home.php
@@ -0,0 +1,36 @@
+                <!--  =======  HOME PAGE ======= -->
+                <div id="pg-home" align="justify">
+                  <div class="page-header">
+                    <h1>Lets get started!</h1>
+                  </div>
+                  <div class="span11" style="padding-bottom:30px">
+                    <strong>Mr. Recruiter</strong> will help you hire the best people around. 
+                    Use the menu on top to do different things. And the best part, all this runs off the cloud. 
+                      Meaning, accessible by whole of your team from anywhere anytime. Have a lot of fun!
+                  </div>
+                  <div class="span3">
+                    <div class="well">
+                      <h2 class="text-info">Search
+                      </h2>
+                      You can search through every word in every resume.
+                      Viewing details of the candidates, and shuffling through
+                      tons of profiles is as easy as it can get. 
+                    </div>
+                  </div>
+                  <div class="span3">
+                    <div class="well">
+                      <h2 class="text-warning">Upload
+                      </h2>
+                      You can drag and drop resume to add them to the database. Every resume
+                      added is indexed and made available for a full text search. 
+                    </div>  
+                  </div>
+                  <div class="span3">
+                    <div class="well">
+                      <h2 class="text-success">Stats
+                      </h2>
+                      What you can't measure, you can't improve. See a range of metrics
+                      and interesting analytics that helps you take key decisions. 
+                    </div>  
+                  </div>
+                </div>          
---
+++ src/views/search.php
@@ -0,0 +1,23 @@
+              <!--  =======  SEARCH PAGE ======= -->
+                <div id="pg-search" style="display:none">
+                  <div class="page-header">
+                    <h1>Search</h1>
+                  </div>
+                  <div id="search-hlp">
+                    <blockquote>
+                      Use the box below to enter a free flowing search string including boolean (and, or). Best matched results will be listed right below. <a id="hide-search-hlp" href="#">[ hide ]</a>
+                    </blockquote>
+                  </div>
+                  <div class="input-append">
+                    <div class="span9">
+                      <input type="text" name="" id="" style="width:580px;font-size:16px">
+                      <span class="add-on">
+                        <i class="icon-search"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <div id="search-results">
+                     <?php include("views/search-results.php") ?> 
+                  </div>
+                </div>
+  
